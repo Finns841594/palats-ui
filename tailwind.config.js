f552c23811aa1d09082f42c8f0de1473e3130330
SMooreSwe <stevedm88@gmail.com>
2023-11-21 14:08:34 +0100

    updating tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 import { SemanticToken } from './tokens/SemanticToken';
 
 module.exports = {
-  content: [
-  "./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",
-  "./stories/**/*.{js,ts,jsx,tsx}",
-  './src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   safelist: [
     {
       pattern: /^(.*?)/,
